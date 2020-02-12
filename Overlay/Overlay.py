e7a8bf0edc8e332b0c617fea1179fc2d2cc61533
Arjun <as3999@drexel.edu>
2020-02-12 16:42:54 -0500

    Overlay test 2

--- Overlay/Overlay.py
+++ Overlay/Overlay.py
@@ -16,24 +16,33 @@ def changeImageSize(maxWidth,
     
 # Take two images for blending them together   
 image1 = Image.open("frame_82.JPG")
+
 image2 = Image.open("frame82.JPG")
+width, height = image2.size
+
+print(height,width)
+
+image2 = image2.crop((0, 461, width-0, height-0))
+print(image2.size)
+#image3.save("new.png","PNG")
 
 # Make the images of uniform size
-image3 = changeImageSize(1640, 922, image1)
-image4 = changeImageSize(1640, 922, image2)
+image3 = changeImageSize(1640, 461, image1)
+image4 = changeImageSize(1640, 461, image2)
 
 # Make sure images got an alpha channel
-image5 = image3.convert("RGBA")
-image6 = image4.convert("RGBA")
+image5 = image1.convert("RGBA")
+image6 = image2.convert("RGBA")
 
 # Display the images
-image5.show()
-image6.show()
+image1.show()
+image2.show()
 
 # alpha-blend the images with varying values of alpha
-alphaBlended1 = Image.blend(image5, image6, alpha=.2)
-alphaBlended2 = Image.blend(image5, image6, alpha=.4)
+alphaBlended1 = Image.blend(image5, image6, alpha=.4)
+alphaBlended2 = Image.blend(image5, image6, alpha=.2)
 
 # Display the alpha-blended images
 alphaBlended1.show()
 alphaBlended2.show()
+
